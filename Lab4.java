2a9a7b53c68d3c084597f145d7a6499ef0498bb0
hannahrappaport <51413749+hannahrappaport@users.noreply.github.com>
2019-06-14 07:38:53 -0400

    Update Lab4.java

--- Lab4.java
+++ Lab4.java
@@ -19,13 +19,24 @@ public class Lab4
     Robot.move();
     turnAround();
   }
-  
+  public static void onerow()
+  { Robot.turnLeft();
+      while(Robot.onDark() == false) {
+          Robot.makeDark();
+          Robot.move();}
+      turnAround();
+      while(Robot.frontIsClear() == true) {
+          Robot.move(); }
+          Robot.turnLeft();
+    
+      
+    }
   public static void completeBars()
   {
-    //insert instructions below
-
-    
-    
+    while(Robot.frontIsClear() == true) {
+        onerow();
+    Robot.move();}
+        onerow();
   }
   
   public static void testCompleteBars1()
@@ -43,13 +54,58 @@ public class Lab4
   }
   
   public static void combinePiles()
+  { while (Robot.onDark()) { 
+      oneSquare();
+      doitagain();
+            } 
+      
+      if (Robot.frontIsClear()) {
+            while (Robot.onDark()) {
+                oneSquare();
+                doitagain(); }}
+      
+        }
+   
+    
+  
+  public static void doitagain() 
+  { turnAround();
+      while (Robot.frontIsClear()) {
+          Robot.move();}
+          Robot.turnLeft();
+          Robot.move();
+          Robot.turnLeft();
+          while (Robot.frontIsClear()) {
+              Robot.move();}
+               turnAround();
+               while (Robot.onDark() == false && Robot.frontIsClear()) {Robot.move();}}
+public static void oneSquare()
+{ if (Robot.onDark() == true) {
+      Robot.makeLight();
+      while (Robot.frontIsClear()) {
+          Robot.move(); }
+          turnRight();
+          Robot.move();
+          turnRight();
+          while (Robot.onDark() == false && Robot.frontIsClear() == true ) {
+              Robot.move(); }
+              if (Robot.onDark()) {backUp();}
+              Robot.makeDark(); }
+}
+  public static void testCombinePiles1()
   {
-    //insert instructions below
-
-    
-    
+    Robot.load("piles1.txt");
+    Robot.setDelay(0.05);
+    combinePiles();
   }
-
+  
+  public static void testCombinePiles2()
+  {
+    Robot.load("piles2.txt");
+    Robot.setDelay(0.05);
+    combinePiles();
+  }
+  
   public static void testCombinePiles1()
   {
     Robot.load("piles1.txt");
